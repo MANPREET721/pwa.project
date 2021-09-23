c3218d475583cd96abeb957ec3a72c9d456a4c3c
MANPREET721 <91246820+MANPREET721@users.noreply.github.com>
2021-09-23 14:39:27 +0530

    Add files via upload

---
+++ precache-manifest.9291d51796ce8c37d43b5bf33a002fab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d139a7b113b5b2635f4",
+    "url": "/css/app.d40fc157.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2a905ab28e341367b84b7e375fe2d77e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d139a7b113b5b2635f4",
+    "url": "/js/app.d2ea140e.js"
+  },
+  {
+    "revision": "07a91c1c6c7e7f736f3a",
+    "url": "/js/chunk-vendors.1eaad8a1.js"
+  },
+  {
+    "revision": "f0bcded14c4758414452d4e15855e170",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
